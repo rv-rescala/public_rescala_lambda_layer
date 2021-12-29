daf7b398198dabe9a4109460ab6fae998b4f3c5e
EC2 Default User <ec2-user@ip-172-31-11-17.ap-northeast-1.compute.internal>
2021-12-29 16:04:45 +0000

    update

--- py37-browser/mk_layer.sh
+++ py37-browser/mk_layer.sh
@@ -1,10 +1,10 @@
 rm -rf browser
 mkdir browser
 cd browser
-wget https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-37/stable-headless-chromium-amazonlinux-2017-03.zip
+wget https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-55/stable-headless-chromium-amazonlinux-2017-03.zip
 unzip stable-headless-chromium-amazonlinux-2017-03.zip
 rm -rf stable-headless-chromium-amazonlinux-2017-03.zip
-wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/2.43/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 rm -rf chromedriver_linux64.zip
 wget https://moji.or.jp/wp-content/ipafont/IPAexfont/IPAexfont00401.zip --no-check-certificate
